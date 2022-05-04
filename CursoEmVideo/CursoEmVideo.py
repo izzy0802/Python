f48a2d935745ded785ca252c0629f989306e63fc
Isis Tristao Santos <83311502+izzy0802@users.noreply.github.com>
2022-05-04 09:38:46 -0300

    Add files via upload

---
+++ CursoEmVideo/CursoEmVideo.py
@@ -0,0 +1,32 @@
+#print("Hello World!")
+#print(7+4)
+#print('7'+'4')
+#print('Ola'+5) vai da errado pq ele nao soma tipos diferentes entao o correto é print('Ola',5)
+
+#nome = 'isis'
+#idade = 21
+#peso = 57
+
+#print(nome,idade,peso)
+
+#nome = input('Qual o seu nome?')
+#idade = input('Qual a sua idade?')
+#peso = input('Qual o seu peso?')
+
+#print(nome,idade,peso)
+
+#nome = input('Qual o seu nome?')
+#print('Olá',nome,'!','Prazer em te conhecer!')
+
+#dia = input('Digite o dia que voce nasceu')
+#mes = input('Digite o mes que voce nasceu')
+#ano = input('Digite o ano que voce nasceu')
+
+#print('Voce nasceu no dia',dia,'de',mes,'de',ano,'.','Correto?')
+
+numero1 = int(input('Digite o primeiro numero '))
+numero2 = int(input('Digite o segundo numero '))
+total = numero1+numero2
+
+#print('A soma entre',numero1,'e',numero2,'vale {}'.format(total))
+print('A soma entre {} e {} vale {}'.format(numero1,numero2,total))
